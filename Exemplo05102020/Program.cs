ab327487a5a0ed6ea994250045006b41de40c425
luiscandidohonorio <63297583+luiscandidohonorio@users.noreply.github.com>
2020-10-05 14:26:15 -0300

    programa e class atualizados

---
+++ Exemplo05102020/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace Exemplo180042___05_10_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+
+            p.X = 10;
+            p.Y = 20;
+            
+            Console.WriteLine(p);
+            
+            p = new Point();
+            Console.WriteLine(p); 
+        }
+    }
+}
---
+++ Exemplo180042 - 05-10-2020/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course
+{
+	struct Point
+	{
+		public double X, Y;
+
+		public override string ToString()
+		{
+			return "(" + X + "," + Y + ")";
+		}
+	}
+}	
