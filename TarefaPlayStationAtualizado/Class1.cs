0834b6c56e5eddaf4c450c7c5036a38675d2d226
luiscandidohonorio <63297583+luiscandidohonorio@users.noreply.github.com>
2020-10-12 14:52:50 -0300

    atividade 12-10-2020 (aniversario)

---
+++ TarefaPlayStationAtualizado/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefaPlayStationAtualizado
+public class Produto
+{
+	public string Nome;
+	public double Preco;
+	public int Quantidade;
+	public double ValorTotalEmEstoque()
+	{
+		returnPreco* Quantidade;
+	}
+	public void AdicionarProduto(intquantidade)
+	{
+		Quantidade += quantidade;
+	}
+	public void RemoverProdutos(intquantidade)
+	{
+		Quantidade = Quantidade - quantidade;
+	}
+	public override string ToString()
+	{
+		return Nome
+		+ ", R$ "
+		+ Preco.ToString("F2", CultureInfo.InvariantCulture)
+		+ ", "
+		+ Quantidade
+		+ " unidades, Total: R$ "
+		+ ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+	}
+}
+	
---
+++ TarefaPlayStationAtualizado/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefaPlayStationAtualizado
+public class Program
+{
+	static void Main(string[] arg)
+	{
+		Produto p = new Produto();
+		Console.WriteLine("Entre com os dados do produto;");
+		Console.Write("Nome: ");
+		p.Nome = Console.ReadLine();
+		Console.Write("Preço: ");
+		p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+		Console.Write("Quantidade no estoque: ");
+		p.Quantidade = int.Parse(Console.ReadLine());
+		Console.WriteLine("Dados do produto: " + p);
+		Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+		int qte = int.Parse(Console.ReadLine());
+		p.AdicionarProdutos(qte);
+		Console.WriteLine("Dados do atualizados: " + p);
+		Console.WriteLine("Digite o número de produtos a serem removidos do estoque: ");
+		qte = int.Parse(Console.ReadLine());
+		p.RemoverProdutos(qte);
+		Console.WriteLine("Dados do atualizados: " + p);
+	}
