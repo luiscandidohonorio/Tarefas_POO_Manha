2cf139f73288566a115cfef8648387275c1a077b
luiscandidohonorio <63297583+luiscandidohonorio@users.noreply.github.com>
2020-10-12 18:49:59 -0300

    atividade 12-10-2020

---
+++ Tarefa12-10-2020/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Produto
+{
+    //Declaração de variavéis
+    private string_nome;
+    private double_preco;
+    private int_quantidade;
+    //Construtor padrão
+    public Produto ( ){
+                    }
+    //Construtor personalizado
+    public Produto (stringnome, doublepreco, int quantidade){_
+        nome = nome;
+        _preco= preco;
+        _quantidade = quantidade;
+}
+   //Utilização do GET e SET
+    public stringGetNome() {
+    return_nome;
+}
+   //Critérios de entrada de dados
+    public voidSetNome(stringnome) {
+    if(nome != null&& nome.Lenght> 1) {
+        _nome = nome;
+    }
+}
+    /* Retorno dos valores Preço do produto e Quantidade do produto sem interferência do usuário */
+    public double GetPreco()
+    {
+        return _preco;
+    }
+    public double GetQuantidade()
+    {
+        return _preco;
+    }
---
+++ Tarefa12-10-2020/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarefa12_10_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Produto p = new Produto(“PS5”, 6000.00, 10);
+                p.nome = “XBOX X”;
+                p.SetNome(“XBOX X”);
+                p.SetPreco (5000.00); Não consigo inserir tais informações para segurança
+                p._preco = 5000.00;
+                Console.WriteLine(p.GetNome());
+                Console.WriteLine(p.GetPreco());
+            
+        }
+    }
+   
+
