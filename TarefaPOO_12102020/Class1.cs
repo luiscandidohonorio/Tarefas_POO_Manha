8eb43ab492533834ed1ea5db8838144ef1fc17b1
luiscandidohonorio <63297583+luiscandidohonorio@users.noreply.github.com>
2020-10-12 11:54:12 -0300

    atividade 12-10-2020 (presente de aniversário...)

---
+++ TarefaPOO_12102020/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefaPOO_12102020
+{
+ class Program {
+        static void Main(string[] args) {     
+        //Criando a variável de entrada
+        int n = int.Parse(Console.ReadLine());
+        //Criando vetor com o número da variável de entrada 
+        double[] vect= new double[n];
+        //Utilização do comando for para percorrer o vetor
+        for (int i  = 0; i < n; i++) 
+            {
+            //Entrada dos elementos no vetor pelo usuário
+            vect[ i ] = double.Parse(Console.ReadLine()), CultureInfo.InvariantCulture); 
+            }
+    //Variável soma       
+    double sum = 0.0;
+    //Utilização do for para percorrer o vetor
+    for (int i = 0; i < n; i ++) {
+        //Somatoriados elementos inseridos 
+        sum += vect[ i ];    
+        }
+
+        }
+}
---
+++ TarefaPOO_12102020/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefaPOO_12102020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double maior = double.MinValue, menor = double.MaxValue;
+            double mediaHomens = 0.0f, mediaMulheres = 0.0f;
+            int contadorH = 0, contadorF = 0;
+            int pessoasLength = 50;
+            for (int i = 0; i < pessoasLength; i++)
+            {
+                Console.Clear();
+                Console.WriteLine("Digite o sexo da pessoa {0}:\n1 - Homem\n2 - Mulher", i + 1);
+                int s = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite a  altura da pessoa:");
+                double altura = double.Parse(Console.ReadLine());
+                if (altura > maior)
+                {
+                    maior = altura; // se for maior que a altura, pegue-a
+                }
+                if (altura < menor)
+                {
+                    menor = altura; // o mesmo para a menor
+                }
+                if (s == 1)
+                {
+                    // é homem
+                    mediaHomens += altura;
+                    contadorH++;
+                }
+                else
+                {
+                    // é mulher
+                    mediaMulheres += altura;
+                    contadorF++;
+                }
+            }
+            // depois de ler as 50 pessoas...
+            // calcular a média:
+            mediaHomens /= contadorH;
+            mediaMulheres /= contadorF; // divide por 50
+
+            Console.Clear();
+            Console.WriteLine("===========================");
+            Console.WriteLine("Maior altura: {0}", maior);
+            Console.WriteLine("Menor altura: {0}", menor);
+            Console.WriteLine("Média da altura masculina: {0}", mediaHomens);
+            Console.WriteLine("Média da altura feminina: {0}", mediaMulheres);
+            Console.ReadKey();
+        }
+    }
+}
