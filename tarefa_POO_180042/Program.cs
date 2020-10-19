551fb6d78b15c4bc278c9a3e3463563d2743f563
luiscandidohonorio <63297583+luiscandidohonorio@users.noreply.github.com>
2020-10-19 09:54:09 -0300

    exemplo 19-10-2020

---
+++ tarefa_POO_180042/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarefa_POO_180042
+{
+    class Program{
+        static void Main(string[] args) {
+            //Criando a variável de entrada
+            int n = int.Parse(Console.ReadLine());
+            //Criando vetor com o número da variável de entrada
+            Produto [] vect= new Produto [n];
+            //Utilização do comando for para percorrer o vetor
+            for (int i  = 0; i < n; i++) {
+
+                string name= Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);         
+                //Entrada dos elementos no vetor pelo usuário
+                vect[ i ] = new Produto {Name= name, Price= price}; 
+            }
+            //Variável soma       
+            double sum = 0.0;
+            //Utilização do for para percorrer o vetor
+            for (int i = 0; i < n; i ++) {
+                //Somatoriados elementos inseridos 
+                sum += vect[ i ].Price;    
+            }
+        }
+    }
+}
